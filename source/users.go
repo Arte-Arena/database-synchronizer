c818511ea69238e6ecbc06abb4213dfb2bb47061
Pablo Yuri <pabloyuridev@gmail.com>
2025-05-15 10:26:46 -0300

    Add MYSQLUsers struct

--- source/users.go
+++ source/users.go
@@ -5,10 +5,25 @@ import (
 	"database_sync/source/database"
 	"database_sync/source/utils"
 	"os"
+	"time"
 
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
+
+type MongoDBUsers struct {
+}
+
+type MYSQLUsers struct {
+	ID              uint64    `db:"id"`
+	Name            string    `db:"name"`
+	Email           string    `db:"email"`
+	EmailVerifiedAt time.Time `db:"email_verified_at"`
+	Password        string    `db:"password"`
+	RememberToken   string    `db:"remember_token"`
+	CreatedAt       time.Time `db:"created_at"`
+	UpdatedAt       time.Time `db:"updated_at"`
+}
 
 func SyncUsers() {
 	ctx, cancel := context.WithTimeout(context.Background(), database.MONGODB_TIMEOUT)
