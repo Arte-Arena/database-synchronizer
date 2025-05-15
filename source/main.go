d9a42f1e827cdec4656bf80fd989a6531412a0df
Pablo Yuri <pabloyuridev@gmail.com>
2025-05-15 12:33:37 -0300

    Add ticker in main function

--- source/main.go
+++ source/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"database_sync/source/utils"
+	"fmt"
+	"log"
+	"time"
+)
 
+func main() {
+	utils.LoadEnvVariables()
+
+	if err := SyncUsers(); err != nil {
+		log.Printf("Error synchronizing users: %v", err)
+	} else {
+		fmt.Println("Initial user synchronization completed successfully")
+	}
+
+	ticker := time.NewTicker(5 * time.Minute)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		fmt.Println("Running scheduled user synchronization...")
+		if err := SyncUsers(); err != nil {
+			log.Printf("Error synchronizing users: %v", err)
+		} else {
+			fmt.Println("User synchronization completed successfully")
+		}
+	}
 }
