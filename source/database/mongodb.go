a990b6dac4e160ad4d92e9e977947de26902a4ca
Pablo Yuri <pabloyuridev@gmail.com>
2025-05-15 09:45:39 -0300

    Add MongoDB configs

--- source/database/mongodb.go
+++ source/database/mongodb.go
@@ -1,1 +1,29 @@
 package database
+
+import (
+	"database_sync/source/utils"
+	"os"
+	"time"
+)
+
+func GetDB() string {
+	environment := os.Getenv(utils.ENV)
+
+	if environment == utils.ENV_RELEASE {
+		return utils.ENV_RELEASE
+	}
+
+	if environment == utils.ENV_HOMOLOG {
+		return utils.ENV_HOMOLOG
+	}
+
+	if environment == utils.ENV_DEVELOPMENT {
+		return utils.ENV_DEVELOPMENT
+	}
+
+	panic("[MongoDB] Invalid DB name")
+}
+
+const (
+	MONGODB_TIMEOUT = 20 * time.Second
+)
