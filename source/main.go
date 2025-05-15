4948a5ce542b45eef0f17a5dbdec87e86b07ca03
Pablo Yuri <pabloyuridev@gmail.com>
2025-05-15 15:33:13 -0300

    Improve main file

--- source/main.go
+++ source/main.go
@@ -4,27 +4,62 @@ import (
 	"database_sync/source/utils"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 )
 
 func main() {
 	utils.LoadEnvVariables()
 
-	if err := SyncUsers(); err != nil {
-		log.Printf("Error synchronizing users: %v", err)
-	} else {
-		fmt.Println("Initial user synchronization completed successfully")
-	}
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		if err := SyncUsers(); err != nil {
+			log.Printf("Error synchronizing users: %v", err)
+		} else {
+			fmt.Println("Initial user synchronization completed successfully")
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		if err := SyncLeads(); err != nil {
+			log.Printf("Error synchronizing leads: %v", err)
+		} else {
+			fmt.Println("Initial leads synchronization completed successfully")
+		}
+	}()
+
+	wg.Wait()
 
 	ticker := time.NewTicker(5 * time.Minute)
 	defer ticker.Stop()
 
 	for range ticker.C {
-		fmt.Println("Running scheduled user synchronization...")
-		if err := SyncUsers(); err != nil {
-			log.Printf("Error synchronizing users: %v", err)
-		} else {
-			fmt.Println("User synchronization completed successfully")
-		}
+		wg.Add(2)
+
+		go func() {
+			defer wg.Done()
+			fmt.Println("Running scheduled user synchronization...")
+			if err := SyncUsers(); err != nil {
+				log.Printf("Error synchronizing users: %v", err)
+			} else {
+				fmt.Println("User synchronization completed successfully")
+			}
+		}()
+
+		go func() {
+			defer wg.Done()
+			fmt.Println("Running scheduled leads synchronization...")
+			if err := SyncLeads(); err != nil {
+				log.Printf("Error synchronizing leads: %v", err)
+			} else {
+				fmt.Println("Leads synchronization completed successfully")
+			}
+		}()
+
+		wg.Wait()
 	}
 }
