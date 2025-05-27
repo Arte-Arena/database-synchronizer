59512b6f0b36ff3f884303794e6d4aeb79f0b83b
Pablo Yuri <pabloyuridev@gmail.com>
2025-05-27 14:28:30 -0300

    Add init container

---
+++ source/init-container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "" >> .env
+echo "MONGODB_URI=$MONGODB_URI" >> .env
+echo "ENV=$ENV" >> .env
+echo "MYSQL_URI=$MYSQL_URI" >> .env
+
+
+echo "[arte arena security] Configurando variáveis de ambiente..."
+
+/app/main
+tail -f /dev/null
